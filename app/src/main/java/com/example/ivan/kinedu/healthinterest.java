0e175dbe2b65b7505fc6951d3b44a7be5371b1dd
IvanLugo <jesus.i.lugo@hotmail.com>
2018-06-10 14:11:07 -0500

    Diseño Interests

--- app/src/main/java/com/example/ivan/kinedu/healthinterest.java
+++ app/src/main/java/com/example/ivan/kinedu/healthinterest.java
@@ -2,12 +2,15 @@ package com.example.ivan.kinedu;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageButton;
+
 
 public class healthinterest extends AppCompatActivity {
-
+    ImageButton BreastFeedingImg;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_healthinterest);
     }
+
 }
